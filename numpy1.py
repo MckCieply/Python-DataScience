ea0bac608ac7a008db7a31668faa91afb7a88342
Alex <mwppl123@gmail.com>
2023-03-04 17:05:49 +0100

    Calculating BMIS

--- numpy1.py
+++ numpy1.py
@@ -1,4 +1,7 @@
-names=["Basia","Ania","Grzesiek","Elvira","Katherine"]
-heights=[1.73,1.61,1.80,1.58,1.77]# in metres
-weights=[61,76,78,58,57]# in kilograms
-bmis=[]
+import numpy as np
+
+names=np.array(["Basia","Ania","Grzesiek","Elvira","Katherine"])
+heights=np.array([1.73,1.61,1.80,1.58,1.77])# in metres
+weights=np.array([61,76,78,58,57])# in kilograms
+bmis=weights / (heights*heights)
+print (bmis)
