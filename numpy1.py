849f910567e72fee42336e3e41dccd830c4a0507
Alex <mwppl123@gmail.com>
2023-03-04 17:17:09 +0100

    developing plot and starting printing out formated data

--- numpy1.py
+++ numpy1.py
@@ -1,7 +1,11 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 names=np.array(["Basia","Ania","Grzesiek","Elvira","Katherine"])
 heights=np.array([1.73,1.61,1.80,1.58,1.77])# in metres
 weights=np.array([61,76,78,58,57])# in kilograms
 bmis=weights / (heights*heights)
-print (bmis)
+plt.bar(names, bmis, color="green")
+plt.title("My friends' BMIs")
+plt.show()
+for person in enumerate(names):
+    print(f"## {person[1]:<13}has BMI of {round(bmis[person[0]], 2)} which is ")
